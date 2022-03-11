40ef3faa3cdeb6ebe7e24624f16c39bbad5f31e2
admin <admin@localhost>
2022-03-10 19:56:10 -0600

    Initial Commit

---
+++ LargeScaleProgrammingAssignments/src/org/howard/edu/lsp/assignment5/IntegerSetException.java
@@ -0,0 +1,9 @@
+package org.howard.edu.lsp.assignment5;
+
+public class IntegerSetException extends Exception{
+    
+    public IntegerSetException(){
+        super("This IntegerSet is empty!");
+    }
+    
+}
