6e50b62c5e7fd5b06992c17dab8cc669665a608c
admin <admin@localhost>
2022-04-22 13:30:05 -0500

    Final

---
+++ LargeScaleProgrammingAssignments/src/org/howard/edu/lsp/finalexam/CommonKeyValuePairs.java
@@ -0,0 +1,23 @@
+package org.howard.edu.lsp.finalexam;
+
+public static int CommonKeyValuePairs {
+	 HashMap<String, String> map1,
+     HashMap<String, String> map2
+) throws NullMapException {
+ int count = 0;
+
+ if(map1.isEmpty() || map2.isEmpty()) {
+     throw new NullMapException("One or both maps are null!");
+ }
+
+ for (Map.Entry<String, String> keyValue : map1.entrySet()) {
+     if (map2.containsKey(keyValue.getKey())) {
+         count++;
+     }
+     if(map2.containsValue(keyValue.getValue())) {
+         count++;
+     }
+ }
+ return count;
+}
+}
